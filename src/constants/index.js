713f8775936b9bf3ce44c57cc372d9217f332430
Emil Abbasov <emilabb13@gmail.com>
2024-11-10 18:44:48 +0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import project1 from '../assets/projects/project-1.png';
 import project2 from '../assets/projects/project-2.png';
 import project3 from '../assets/projects/project-3.png';
 import project4 from '../assets/projects/project-4.png';
-import project5 from '../assets/projects/mapspc.PNG';
+import project5 from '../assets/projects/project-5.png';
 
 export const HERO_CONTENT = `I'm a passionate 18-year-old Front-End Developer with a strong foundation in HTML, CSS, and JavaScript. I'm eager to contribute my skills to creating intuitive and visually appealing web experiences. I'm currently learning React and I am always seeking opportunities to expand my knowledge and stay updated on the latest web development trends.`;
 
